26a44902adb7ac627c0620533c3011ac47736da0
Alexandra Kuznetsova <alexminsk.noir@gmail.com>
2017-09-20 18:42:32 +0300

    guessing-game.js has been completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.leftBorder = 0;
+      this.rightBorder = 0;
+      this.medium = 0;
+    }
 
     setRange(min, max) {
-
+      this.leftBorder = min;
+      this.rightBorder = max;
+      this.chooseMedium(min, max);
     }
 
     guess() {
-
+      return this.medium;
     }
 
     lower() {
-
+      this.rightBorder = this.medium;
+      this.chooseMedium(this.leftBorder, this.rightBorder);
     }
 
     greater() {
+      this.leftBorder = this.medium;
+      this.chooseMedium(this.leftBorder, this.rightBorder);
+    }
 
+    chooseMedium(min, max) {
+
+        this.medium = Math.round((max - min) / 2) + min;
     }
 }
 
